90ae280fcd073d9f6fa7384d1fb8a7e4bc45a18c
Diana Sibagatullina <diana.sibagatullina96@gmail.com>
2022-07-17 01:56:59 +0300

    RBTree and Iterators in different files

--- src/TreeIterator.hpp
+++ src/TreeIterator.hpp
@@ -4,15 +4,27 @@
 // https://www.cs.odu.edu/~zeil/cs361/latest/Public/treetraversal/index.html
 // https://cplusplus.com/reference/iterator/BidirectionalIterator/
 
-# include "utils.hpp"
-
+// # include "RedBlackTree.hpp"
+# include "node.hpp"
 namespace ft {
 
 /*--------------------------------------- random_access_iterator ---------------------------------------*/
 template <class T>
-class TreeIterator : public std::iterator<std::bidirectional_iterator_tag, T>
+class TreeIterator : public ft::iterator< std::bidirectional_iterator_tag, T>
 {
 public:
+
+public:    
+    // nodePtr is the current location in the tree. we can move
+    // freely about the tree using left, right, and parent.
+    // root - root pointer of the tree, which is needed for ++ and --
+    // when the iterator value is end()
+    Node<T> *nodePtr;
+	Node<T> *root;
+    
+    // used to construct an iterator return value from a node pointer
+    TreeIterator (const Node<T> *p, const Node<T> *r) : nodePtr(p), root(r) {}
+	
 	TreeIterator() {}
 	TreeIterator(const TreeIterator& other) : nodePtr(other.nodePtr), root(other.root) {}
 
@@ -101,16 +113,98 @@ public:
 		return (clone);
 	}
 
-private:    
-    // nodePtr is the current location in the tree. we can move
-    // freely about the tree using left, right, and parent.
-    // root - root pointer of the tree, which is needed for ++ and --
-    // when the iterator value is end()
-    Node<T> *nodePtr;
-	Node<T> *root;
+};
+
+template <class T>
+class TreeConstIterator : public ft::iterator<std::bidirectional_iterator_tag, T, std::ptrdiff_t, const T*, const T&>
+{
+public:
+	TreeConstIterator() {}
+	TreeConstIterator(const TreeConstIterator& other) : nodePtr(other.nodePtr), root(other.root) {}
+
+	TreeConstIterator &operator=(const TreeConstIterator& other) {
+		nodePtr = other.nodePtr;
+		root = other.root;
+		return (*this);
+	}
+
+	~TreeConstIterator() {}
+
+	bool operator==(const TreeConstIterator& rhs) const {return (nodePtr == rhs.nodePtr);}
     
-    // used to construct an iterator return value from a node pointer
-    TreeIterator (const Node<T> *p, const Node<T> *r) : nodePtr(p), root(r) {}
+    bool operator!=(const TreeConstIterator& rhs) const {return (nodePtr != rhs.nodePtr);}
+    
+    const T& operator*() const {return nodePtr->_Myval;}
+    
+	const T* operator->() const {return &(nodePtr->_Myval);}
+
+    TreeConstIterator& operator++() {
+		if (nodePtr == NULL)
+		{
+			if (root == NULL)
+				return (*this);
+			nodePtr = root;
+			while (nodePtr->_Left != NULL)
+				nodePtr = nodePtr->_Left;
+		}
+		else {
+			if (nodePtr->_Right != NULL)
+			{
+				nodePtr = nodePtr->_Right;
+				while (nodePtr->_Left != NULL)
+					nodePtr = nodePtr->_Left;
+			}
+			else {
+				while (nodePtr->_Parent && nodePtr == nodePtr->_Parent->_Right)
+					nodePtr = nodePtr->_Parent;
+				nodePtr = nodePtr->_Parent;
+			}
+		}
+		return (*this);
+	}
+    
+    TreeConstIterator operator++(int) {
+		TreeConstIterator clone(*this);
+		++(*this);
+		return (clone);
+	}
+    
+    TreeConstIterator operator--() {
+		if (nodePtr == NULL)
+		{
+			if (root == NULL)
+				return (*this);
+			nodePtr = root;
+			while (nodePtr->_Right != NULL)
+				nodePtr = nodePtr->_Right;
+		}
+		else {
+			if (nodePtr->_Left != NULL)
+			{
+				nodePtr = nodePtr->_Left;
+				while (nodePtr->_Right != NULL)
+					nodePtr = nodePtr->_Right;
+			}
+			else {
+				while (nodePtr->_Parent && nodePtr == nodePtr->_Parent->_Left)
+					nodePtr = nodePtr->_Parent;
+				nodePtr = nodePtr->_Parent;
+			}
+		}
+		return (*this);
+	}
+    
+    TreeConstIterator operator--(int) {
+		TreeConstIterator clone(*this);
+		--(*this);
+		return (clone);
+	}
+
+public:    
+    const Node<T> *nodePtr;
+	const Node<T> *root;
+    
+    TreeConstIterator (const Node<T> *p, const Node<T> *r) : nodePtr(p), root(r) {}
 };
 
 // /*--------------------------------------- rev_random_access_iterator ---------------------------------------*/
