8a335f5d786fe81c6ff66be1faae8e6cb3e6befd
Chelsey Overandout <coverand@at-q8.msk.21-school.ru>
2022-07-20 19:48:33 +0300

    comparison operators for pairs changed

--- src/utils.hpp
+++ src/utils.hpp
@@ -169,7 +169,7 @@ struct pair
     //     return (*this);
     // }
 };
-
+#include <utility>
 /* Creates a pair object, deducing the target type from the types of arguments. */
 template< class T1, class T2 >
 pair<T1,T2> make_pair( T1 t, T2 u ) { return pair<T1,T2>(t, u);}
@@ -182,21 +182,17 @@ bool operator!=( const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs ) {retur
 
 template< class T1, class T2 >
 bool operator<( const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs ) {
-	if (lhs.first < rhs.first)
-		return true;
-	if (lhs.first > rhs.first)
-		return false;
-	return (lhs.second < rhs.second);
+	return (lhs.first < rhs.first || (!(lhs.first < rhs.first) && lhs.second < rhs.second));
 }
 
 template< class T1, class T2 >
-bool operator<=( const std::pair<T1,T2>& lhs, const std::pair<T1,T2>& rhs ) {return (lhs.first < rhs.first || lhs.first == lhs.first);}
+bool operator<=( const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs ) {return !(rhs < lhs);}
 
 template< class T1, class T2 >
-bool operator>( const std::pair<T1,T2>& lhs, const std::pair<T1,T2>& rhs ) {return !(lhs <= rhs);}
+bool operator>( const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs ) {return rhs < lhs;}
 
 template< class T1, class T2 >
-bool operator>=( const std::pair<T1,T2>& lhs, const std::pair<T1,T2>& rhs ) {return !(lhs < rhs);}
+bool operator>=( const ft::pair<T1,T2>& lhs, const ft::pair<T1,T2>& rhs ) {return !(lhs < rhs);}
 
 // https://en.cppreference.com/w/cpp/algorithm/lexicographical_compare
 template<class InputIt1, class InputIt2>
